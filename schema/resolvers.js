d7f475a27afb0414d0b83ebe1af827c29c4347f2
Avindhya Cabral <avindhya.cabral@gmail.com>
2025-11-24 10:36:18 +0530

    mutations: basic

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,4 @@
-const { UserList } = require('../FakeData');
+const { UserList, MovieList } = require('../FakeData');
 const _ = require('lodash');
 
 const resolvers = {
@@ -8,10 +8,54 @@ const resolvers = {
     },
     user: (parent, args) => {
       const id = args.id; // The args parameter is always 2nd. Parent would be first.
-      const user = _.find(UserList, { id }); // if the key and value 'id' are the same you can also just do { id } instead of {id: id}
+      const user = _.find(UserList, { id: Number(id) }); // if the key and value 'id' are the same you can also just do { id } instead of {id: id}
+      return user;
       /* 
       This is where you would do your logic to SELECT id from your DB, if you are working with one
       */
+    },
+
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+    },
+  },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+
+    updateUsername: (parent, args) => {
+      //   const id = args.input.id;
+      //   const newUsername = args.input.newUsername;
+      const { id, newUsername } = args.input; // destructuring to handle multiple variables in one line, instead of the above
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+      return userUpdated;
     },
   },
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -8,11 +8,40 @@ const typeDefs = gql`
     age: Int!
     nationality: Nationality!
     friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]! # List of type User
     user(id: ID!): User!
+    movies: [Movie!]! # List of type Movie
+    movie(name: String!): Movie!
+  }
+
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality = BRAZIL
+    # friends: [User]
+    # favoriteMovies: [Movie]     ----- doing these later seperately so we don't have to state friends and fav movies on input
+  }
+
+  input UpdateUsernameInput {
+    id: ID!
+    newUsername: String!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+    updateUsername(input: UpdateUsernameInput!): User
   }
 
   enum Nationality {
